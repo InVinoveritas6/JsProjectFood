e07873154a8a254f88b821f8a1893ea714e211b3
Alexandr Lagodich <sagodich@mail.ru>
2021-07-15 14:28:03 +0300

    Timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+   //Tabs
    const tabs = document.querySelectorAll('.tabheader__item'),
       tabContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector(".tabheader__items");
@@ -34,4 +35,59 @@ window.addEventListener('DOMContentLoaded', () => {
          });
       }
    });
+
+   //Timer
+
+   const deadLine = '2021-08-11';
+
+   function getTimeRemening(endtime) {
+      const t = Date.parse(endtime) - Date.parse(new Date()),
+         days = Math.floor(t / (1000 * 60 * 60 * 24)),
+         hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+         minutes = Math.floor((t / (1000 * 60)) % 60),
+         seconds = Math.floor((t / 1000) % 60);
+      return {
+         'total': t,
+         'days': days,
+         'hours': hours,
+         'minutes': minutes,
+         'seconds': seconds
+      };
+   }
+
+   function getZiro(num) {
+      if ( num >= 0 && num< 10){
+         return `0${num}`;
+      } else {
+         return num;
+      }
+   }
+   
+   function setClock(selector, endtime) {
+      const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateclock, 1000);
+
+      updateclock();      
+
+      function updateclock() {
+         const t = getTimeRemening(endtime);
+
+         days.innerHTML = getZiro(t.days);
+         hours.innerHTML = getZiro(t.hours);
+         minutes.innerHTML = getZiro(t.minutes);
+         seconds.innerHTML = getZiro(t.seconds);
+         
+         if (t.total <= 0) {
+               clearInterval(timeInterval);
+         }
+      }
+   }
+
+setClock('.timer', deadLine);
+
+
 });
